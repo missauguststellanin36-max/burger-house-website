14a5932591e2eaaa9a0bf8eeb66c21e5d8556851
Phal Sophanin <missauguststellanin36@gmail.com>
2026-08-19 17:25:31 +0700

    Keep Pages workflow compatible with source entry

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'src/index.html',
+      },
+    },
+  },
   base: command === 'build' ? '/burger-house-website/' : '/',
 }))
